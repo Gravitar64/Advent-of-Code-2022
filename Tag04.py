e2d77122edbbff0075d7458cebd533376221556b
Gravitar64 <gravitar@web.de>
2022-12-04 15:10:58 +0100

    youtube

--- Tag04.py
+++ Tag04.py
@@ -1,14 +1,16 @@
 from time import perf_counter as pfc
 import re
 
+
 def read_puzzle(file):
   with open(file) as f:
-    return [map(int,re.findall(r'\d+',line)) for line in f.readlines()]
+    return [list(map(int,re.findall('\d+',line))) for line in f.readlines()]
 
 
 def solve(puzzle):
-  results = [(a <= c and b >=d or c <= a and d >=b, max(a,c) <= min(b,d)) for a,b,c,d in puzzle]
-  return list(map(sum, zip(*results)))
+  sum1 = sum([a <= c and b >= d or c <= a and d >= b for a,b,c,d in puzzle])
+  sum2 = sum([max(a,c) <= min(b,d) for a,b,c,d in puzzle])
+  return sum1, sum2
 
 start = pfc()
 print(solve(read_puzzle('Tag04.txt')))

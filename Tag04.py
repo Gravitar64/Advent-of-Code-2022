dea80c5258d4df75dfbb9644a945391694653b3a
Gravitar64 <gravitar@web.de>
2022-12-04 14:07:22 +0100

    Start-End Vergleich

--- Tag04.py
+++ Tag04.py
@@ -1,24 +1,14 @@
 from time import perf_counter as pfc
-
+import re
 
 def read_puzzle(file):
   with open(file) as f:
-    return [line.strip() for line in f.readlines()]
+    return [map(int,re.findall(r'\d+',line)) for line in f.readlines()]
 
-
-def get_sets(line):
-  pairs = [map(int,pair.split('-')) for pair in line.split(',')]
-  return [set(range(f,t+1)) for f,t in pairs]
-  
 
 def solve(puzzle):
-  sum1 = sum2 = 0
-  for line in puzzle:
-    a, b = get_sets(line)
-    if a.issubset(b) or b.issubset(a): sum1 += 1
-    if set.intersection(a, b):         sum2 += 1
-  return sum1, sum2
-
+  results = [(a <= c and b >=d or c <= a and d >=b, max(a,c) <= min(b,d)) for a,b,c,d in puzzle]
+  return list(map(sum, zip(*results)))
 
 start = pfc()
 print(solve(read_puzzle('Tag04.txt')))
